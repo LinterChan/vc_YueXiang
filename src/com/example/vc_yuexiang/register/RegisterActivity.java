f0acb43105878318ed1186234b82f7699511f6c8
Linter <linterchen@vanchu.net>
2015-11-16 21:10:33 +0800

    修改： 1.使用9图来做注册按钮 2.封装网络请求操作，用HttpRequestHelper完成功能

--- src/com/example/vc_yuexiang/register/RegisterActivity.java
+++ src/com/example/vc_yuexiang/register/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.vc_yuexiang.register;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -14,6 +16,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.vc_yuexiang.R;
+import com.example.vc_yuexiang.http.HttpRequestHelper;
+import com.example.vc_yuexiang.http.HttpRequestHelper.DoResultListener;
 import com.example.vc_yuexiang.http.HttpUtil;
 import com.example.vc_yuexiang.util.TypeUtil;
 
@@ -38,14 +42,41 @@ public class RegisterActivity extends Activity {
 	public void initView() {
 		usernameEditText = (EditText) findViewById(R.id.register_username_edittext);
 		passwordEditText = (EditText) findViewById(R.id.register_password_edittext);
+		usernameEditText.clearFocus();
 	}
 
 	public void onClick_registerButton(View view) {
 		String username = usernameEditText.getText().toString().trim();
 		String password = passwordEditText.getText().toString().trim();
 		if (isCorrectOfInput(username, password)) {
-			RegisterTask registerTask = new RegisterTask();
-			registerTask.execute(username, password);
+			String url = HttpUtil.URL_IP + "/RegisterServlet";
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("username", username);
+			map.put("password", password);
+			HttpRequestHelper helper = new HttpRequestHelper(url, map);
+			helper.setDoResultListener(new DoResultListener() {
+				@Override
+				public void doResult(String result) {
+					switch (Integer.parseInt(result)) {
+					case TypeUtil.REG_USER_EXIST:
+						// 该用户已存在
+						Toast.makeText(RegisterActivity.this, "该用户已存在",
+								Toast.LENGTH_SHORT).show();
+						break;
+					case TypeUtil.REG_SUCCESS:
+						// 注册成功
+						// 跳转到LoginActivity
+						break;
+					case TypeUtil.REG_FAIL:
+						// 注册失败
+						Toast.makeText(RegisterActivity.this, "注册失败,请稍后重试",
+								Toast.LENGTH_SHORT).show();
+						break;
+					}
+				}
+			});
+//			RegisterTask registerTask = new RegisterTask();
+//			registerTask.execute(username, password);
 		}
 	}
 
@@ -69,37 +100,37 @@ public class RegisterActivity extends Activity {
 	 * @author LinterChen linterchen@vanchu.net
 	 * @date 2015-11-16
 	 */
-	class RegisterTask extends AsyncTask<String, Void, Integer> {
-		@Override
-		protected Integer doInBackground(String... param) {
-			// 执行注册操作的url
-			String url = HttpUtil.URL_IP + "/RegisterServlet";
-			List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
-			nameValuePair.add(new BasicNameValuePair("username", param[0]));
-			nameValuePair.add(new BasicNameValuePair("password", param[1]));
-			String result = new HttpUtil().Connect(url, nameValuePair);
-			return Integer.parseInt(result.trim());
-		}
-
-		@Override
-		protected void onPostExecute(Integer result) {
-			switch (result) {
-			case TypeUtil.REG_USER_EXIST:
-				// 该用户已存在
-				Toast.makeText(RegisterActivity.this, "该用户已存在",
-						Toast.LENGTH_SHORT).show();
-				break;
-			case TypeUtil.REG_SUCCESS:
-				// 注册成功
-				// 跳转到LoginActivity
-				break;
-			case TypeUtil.REG_FAIL:
-				// 注册失败
-				Toast.makeText(RegisterActivity.this, "注册失败,请稍后重试",
-						Toast.LENGTH_SHORT).show();
-				break;
-			}
-		}
-	}
+//	class RegisterTask extends AsyncTask<String, Void, Integer> {
+//		@Override
+//		protected Integer doInBackground(String... param) {
+//			// 执行注册操作的url
+//			String url = HttpUtil.URL_IP + "/RegisterServlet";
+//			List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+//			nameValuePair.add(new BasicNameValuePair("username", param[0]));
+//			nameValuePair.add(new BasicNameValuePair("password", param[1]));
+//			String result = new HttpUtil().Connect(url, nameValuePair);
+//			return Integer.parseInt(result.trim());
+//		}
+//
+//		@Override
+//		protected void onPostExecute(Integer result) {
+//			switch (result) {
+//			case TypeUtil.REG_USER_EXIST:
+//				// 该用户已存在
+//				Toast.makeText(RegisterActivity.this, "该用户已存在",
+//						Toast.LENGTH_SHORT).show();
+//				break;
+//			case TypeUtil.REG_SUCCESS:
+//				// 注册成功
+//				// 跳转到LoginActivity
+//				break;
+//			case TypeUtil.REG_FAIL:
+//				// 注册失败
+//				Toast.makeText(RegisterActivity.this, "注册失败,请稍后重试",
+//						Toast.LENGTH_SHORT).show();
+//				break;
+//			}
+//		}
+//	}
 
 }
