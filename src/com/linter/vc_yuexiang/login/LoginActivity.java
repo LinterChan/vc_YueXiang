f5914aa22a2602bbf60d7577c456e23eb604c6f9
Linter <linterchen@vanchu.net>
2015-11-17 11:23:22 +0800

    完成登录模块v1.0

---
+++ src/com/linter/vc_yuexiang/login/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.linter.vc_yuexiang.login;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.vc_yuexiang.R;
+import com.linter.vc_yuexiang.http.HttpRequestHelper;
+import com.linter.vc_yuexiang.http.HttpRequestHelper.DoResultListener;
+import com.linter.vc_yuexiang.http.HttpUtil;
+import com.linter.vc_yuexiang.util.TypeUtil;
+
+public class LoginActivity extends Activity {
+	private EditText usernameEditText;
+	private EditText passwordEditText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		initView();
+	}
+
+	public void initView() {
+		usernameEditText = (EditText) findViewById(R.id.login_username_edittext);
+		passwordEditText = (EditText) findViewById(R.id.login_password_edittext);
+	}
+
+	public void onClick_loginButton(View view) {
+		String username = usernameEditText.getText().toString().trim();
+		String password = passwordEditText.getText().toString().trim();
+		if (isCorrectOfInput(username, password)) {
+			String url = HttpUtil.URL_IP + "/LoginServlet";
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("username", username);
+			map.put("password", password);
+			HttpRequestHelper helper = new HttpRequestHelper(url, map);
+			helper.setDoResultListener(new DoResultListener() {
+				@Override
+				public void doResult(String result) {
+					switch (Integer.parseInt(result)) {
+					case TypeUtil.LOGIN_USER_NOT_EXIST:
+						Toast.makeText(LoginActivity.this, "用户不存在",
+								Toast.LENGTH_SHORT).show();
+						break;
+					case TypeUtil.LOGIN_PASSWORD_WORRY:
+						Toast.makeText(LoginActivity.this, "密码错误",
+								Toast.LENGTH_SHORT).show();
+						break;
+					case TypeUtil.LOGIN_SUCCESS:
+						// 跳转到HomeActivity主页
+						break;
+					case TypeUtil.LOGIN_FAIL:
+						Toast.makeText(LoginActivity.this, "登录失败,请稍后重试",
+								Toast.LENGTH_SHORT).show();
+						break;
+					}
+				}
+			});
+			helper.execute();
+		}
+	}
+
+	public boolean isCorrectOfInput(String username, String password) {
+		if (username.equals("")) {
+			Toast.makeText(LoginActivity.this, "用户名不能为空", Toast.LENGTH_SHORT)
+					.show();
+			return false;
+		}
+		if (password.equals("")) {
+			Toast.makeText(LoginActivity.this, "密码不能为空", Toast.LENGTH_SHORT)
+					.show();
+			return false;
+		}
+		return true;
+	}
+}
--- src/com/linter/vc_yuexiang/util/TypeUtil.java
+++ src/com/linter/vc_yuexiang/util/TypeUtil.java
@@ -19,4 +19,21 @@ public class TypeUtil {
 	 * 注册：注册失败
 	 */
 	public static final int REG_FAIL = 2;
+	
+	/**
+	 * 登录：用户不存在
+	 */
+	public static final int LOGIN_USER_NOT_EXIST = 3;
+	/**
+	 * 登录：密码错误
+	 */
+	public static final int LOGIN_PASSWORD_WORRY = 4;
+	/**
+	 * 登录：登录成功
+	 */
+	public static final int LOGIN_SUCCESS = 5;
+	/**
+	 * 登录：登录失败
+	 */
+	public static final int LOGIN_FAIL = 6;
 }
